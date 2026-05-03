d08a423d31e39bf9ebcc18e4d3563962242ed7a5
joaodouglasdantas <joaodouglasdantas02b@gmail.com>
2026-05-03 18:12:08 -0300

    feat: adicionando phone a tabela de customer

--- EstudoG/db/schema.rb
+++ EstudoG/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_03_195147) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_03_210902) do
   create_table "addresses", force: :cascade do |t|
     t.integer "addressable_id", null: false
     t.string "addressable_type", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_03_195147) do
   create_table "customers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
+    t.string "phone"
     t.datetime "updated_at", null: false
   end
 
