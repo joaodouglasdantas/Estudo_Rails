24b77fd4a67038c417d1f9c4c13b9118275e879b
joaodouglasdantas <joaodouglasdantas02b@gmail.com>
2026-05-03 18:06:42 -0300

    feat: adicionando create no controller

--- EstudoG/app/controllers/customers_controller.rb
+++ EstudoG/app/controllers/customers_controller.rb
@@ -6,4 +6,20 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params) # Parametros do frontend
+
+    if @customer.save
+      redirect_to customers_path, notice: "Cliente criado com sucesso!"
+    else
+      render :new, alert: "Erro ao criar cliente."
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone) # Somente nome e meu phone
+  end
 end
